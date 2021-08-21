14969d7310242d16b791f6c6121f997d7209611d
Anna <a.artamonova2@eapteka.ru>
2021-08-21 08:52:30 +0300

    Ex7

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ print("Hello from Anna")
 
 response = requests.get("https://playground.learnqa.ru/api/get_text")
 print(response.text)
+
+payload = {"name": "User"}
+
+response = requests.get("https://playground.learnqa.ru/api/hello", params=payload)
+print(response.text)
+
