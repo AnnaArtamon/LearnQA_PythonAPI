9fea0c0214e1fd09b67db5128dec9e048d0e00a7
Anna <a.artamonova2@eapteka.ru>
2021-08-25 22:33:52 +0300

    Ex10

---
+++ test_first_api.py
@@ -0,0 +1,6 @@
+
+class FirstTestAPI:
+    def test_insert_short_text(self):
+        phrase = input("Set a phrase: ")
+        length = len(phrase)
+        assert length <= 15, "The phrase is longer than 15 symbols"
